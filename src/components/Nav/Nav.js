745599f179f9b7fc3518ece516746faf48b16e11
chriscasella <chris.casella@gmail.com>
2018-09-29 19:36:44 -0400

    Routing added

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const nav = () => {
+   return(
+       <div className="Nav">
+        Inside Nav!
+       </div>
+   );
+}
+
+export default nav;
---
+++ src/containers/Home/Home.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Nav from '../../components/Nav/Nav';
+
+class Home extends Component {
+    constructor(){
+        super();
+    };
+
+    render(){
+        return(
+        <div>
+            <Nav />
+        </div>
+        )
+    };
+    
+}
+
+export default Home

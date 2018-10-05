e37c154bb38a9d07e55cfc8c13613e4554d50755
chriscasella <chris.casella@gmail.com>
2018-10-04 22:27:11 -0400

    styling

--- src/containers/Home/CryptoListContainer/CryptoListItem/CryptoListItem.js
+++ src/containers/Home/CryptoListContainer/CryptoListItem/CryptoListItem.js
@@ -1,22 +1,26 @@
 import React from 'react';
+import MediaQuery from 'react-responsive';
 
 
 const CryptoListItem = (props) => {
     const coin = props.coinData;
     const roundDown = (num) =>{
-        return Math.floor(num * 10000)/10000;
+        return Math.floor(num * 1000)/1000;
     }
     return(
         <div className="crypto-container__item">
             {coin.cmc_rank}
             <span className="bold-text">{coin.name}</span>
             <span>{coin.symbol}</span>
-            <span className="crypto-container__item-middle ">{roundDown(coin.quote.USD.price)}</span>
-            <span className="crypto-container__item-desktop"> %1hr</span>
-            <span className="crypto-container__item-desktop">{roundDown(coin.quote.USD.percent_change_1h)}</span>
-            <span className="crypto-container__item-desktop">%1Wk</span>
-            <span className="crypto-container__item-desktop">{roundDown(coin.quote.USD.percent_change_7d)}</span>
-            <span className="crypto-container__item-right">%24hr</span>
+            <span className="crypto-container__item-middle ">$ {roundDown(coin.quote.USD.price)}</span>
+            {/* Desktop Stats */}
+            <MediaQuery minDeviceWidth={1024}>
+                <span className="crypto-container__item-desktop"> %1hr</span>
+                <span className="crypto-container__item-desktop">{roundDown(coin.quote.USD.percent_change_1h)}</span>
+                <span className="crypto-container__item-desktop">%1Wk</span>
+                <span className="crypto-container__item-desktop">{roundDown(coin.quote.USD.percent_change_7d)}</span>
+            </MediaQuery>
+            <span className="crypto-container__item-middle">%24hr</span>
             <span className="crypto-container__item-right">{roundDown(coin.quote.USD.percent_change_24h)}</span>
         </div>
     )

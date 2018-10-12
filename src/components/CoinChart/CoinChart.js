892bbeba47080defd32fa88a4777f9057283fc75
chriscasella <chris.casella@gmail.com>
2018-10-11 21:42:45 -0400

    fixed

---
+++ src/components/CoinChart/CoinChart.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import ApexCharts from 'apexcharts';
+
+class CoinChart extends Component {
+    constructor(){
+        super();
+    }
+
+    render(){
+        return(
+            <div>
+                Inside CoinChart!
+            </div>
+        )
+    }
+
+}
+
+export default CoinChart;

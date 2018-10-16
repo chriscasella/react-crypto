a0a3b8f56512e2455871815ac753ae82180fbbce
chriscasella <chris.casella@gmail.com>
2018-10-15 21:29:15 -0400

    search renderin but not working

--- src/components/Navbar/SearchBar/SearchBar.js
+++ src/components/Navbar/SearchBar/SearchBar.js
@@ -1,30 +1,41 @@
 import React, { Component } from 'react';
+import SearchItem from './SearchItem/SearchItem';
 
 class SearchBar extends Component {
     constructor(props){
         super();
         this.state = {
-            selectedCoin: '',
-            coinList : props.coinList,
-            filteredCoins: null
+            typedCoin: '',
+            coinList : props.coinList
         };
+        console.log('these are props', this.state.coinList)
     };
 
     componentDidMount(){
 
     };
 
-    handleSelect(event){
+    handleSelect = (event) =>{
         const val = event.target.value;
-        const filteredResults = this.state.coinList.filter( el => {
-            
-        })
+        this.setState({typedCoin: val});
     };
 
     render(){
+        const copiedCoins = [...this.state.coinList];
+        const filteredCoins = copiedCoins.filter( el => {
+            if(this.state.typedCoin.length > 3){
+               return el.name.search(this.state.typedCoin);
+            }
+        });
+        console.log(filteredCoins)
         return(
             <div>
-                <input onChange={this.handleSelect} />
+                <input value={this.state.typedCoin} onChange={this.handleSelect} />
+                {
+                    filteredCoins.map( el => {
+                        return <SearchItem key={el.id} name={el.name} symbol={el.symbol} />
+                    })
+                }
             </div>
         )
     };

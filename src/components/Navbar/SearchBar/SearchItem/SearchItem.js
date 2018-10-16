7db9542ba9154a67dc93419740c942933f1ef74a
chriscasella <chris.casella@gmail.com>
2018-10-15 22:53:08 -0400

    added routing to search

--- src/components/Navbar/SearchBar/SearchItem/SearchItem.js
+++ src/components/Navbar/SearchBar/SearchItem/SearchItem.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SearchItem = (props) => {
     return(
         <li className="search-list__item">
+        <Link to={"/" + props.symbol} style={{textDecoration: 'none'}}>
              {props.name} ({props.symbol})
+        </Link>
         </li>
     )
 }

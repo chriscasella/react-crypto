4eb16c0b9671d1d8e871f957f0d2ac9726e54722
chriscasella <chris.casella@gmail.com>
2018-10-15 21:55:59 -0400

    styled search to be responsive

--- src/components/Navbar/SearchBar/SearchItem/SearchItem.js
+++ src/components/Navbar/SearchBar/SearchItem/SearchItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 const SearchItem = (props) => {
     return(
         <li className="search-list__item">
-            <h5> {props.name} </h5>
+             {props.name} ({props.symbol})
         </li>
     )
 }

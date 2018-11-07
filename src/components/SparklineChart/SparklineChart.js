397284b75a209cecc2e28bdedc667d9afe6f61e1
chriscasella <chris.casella@gmail.com>
2018-11-06 21:17:12 -0500

    sparkline scaffold

---
+++ src/components/SparklineChart/SparklineChart.js
@@ -0,0 +1,18 @@
+import React, {Component } from 'react';
+
+class SparklineChart extends Component {
+    constructor(props){
+        super();
+
+    }
+
+    render(){
+        return(
+            <div>
+            Inside Sparkline
+            </div>
+        )
+    }
+}
+
+export default SparklineChart;
